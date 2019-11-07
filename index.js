d1d270f042dc5f42744cdf52b4ff8b218fee50f8
Alexander Sierra <alexandercsierra@gmail.com>
2019-11-07 18:11:34 -0500

    buttons change pictures and button margin bigger

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-var button = document.querySelectorAll(".btn");
-var submitBtn = document.querySelector(".submit");
+var sun = document.querySelector(".sun");
+var mount = document.querySelector(".mount");
+var island = document.querySelector(".island");
 var form = document.querySelector(".form");
 var img = document.querySelector(".tripImage");
 var destination = document.querySelector("select");
 
 
 
-for (var i=0; i<button.length; i++){
-    button[i].addEventListener("click", function(e){
+    sun.addEventListener("click", function(e){
         form.classList.remove("hide");
+        img.style.background = 'url("../img/desert.jpg")';
+
     });
-}
+    mount.addEventListener("click", function(e){
+        form.classList.remove("hide");
+        img.style.background = 'url("../img/mount.jpg")';
+
+    });
+    island.addEventListener("click", function(e){
+        form.classList.remove("hide");
+        img.style.background = 'url("../img/island.jpg")';
+
+    });
+
 
 destination.addEventListener("click", function(e){
     var choice = destination.value;
